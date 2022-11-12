71d8303d829a0faec5e4e84b1be3a1891fd4efee
Ihor Melnykov <imelya17@gmail.com>
2022-11-12 15:28:41 +0200

    task2 complete

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,4 +1,7 @@
 /* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 const ingredients = [
 	"Potatoes",
@@ -9,7 +12,30 @@ const ingredients = [
 	"Condiments",
 ];
 
-const elementOne = document.createElement("li");
-// console.log(elementOne); 
-elementOne.textContent = "Potatoes";
-console.log(elementOne);
+
+const listOfIngredients = document.querySelector("#ingredients");
+
+const elements = ingredients.map(ingredient => {
+  const elOfList = document.createElement("li");
+  elOfList.textContent = ingredient;
+    elOfList.classList.add("item");
+  
+  return elOfList;
+});
+
+console.log(elements);
+
+
+listOfIngredients.append(...elements);
+
+
+
+
+
+
+
+
+
+
+
+// listOfIngredients.append(lastItem, ..., ..., ....);
--- src/js/task-03.js
+++ src/js/task-03.js
@@ -1,14 +1,43 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable indent */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 const images = [
-  {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
-  },
-  {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-  },
-  {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
-  },
+	{
+		url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "White and Black Long Fur Cat",
+	},
+	{
+		url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+	},
+	{
+		url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+		alt: "Group of Horses Running",
+	},
 ];
+
+
+const galleryMarkup = document.querySelector(".gallery");
+
+const elements = images.map(image => {
+  const elOfList = document.createElement("img");
+  elOfList.src = ${ image.url };
+  elOfList.alt = ${ image.alt };
+
+  
+  return elOfList;
+});
+
+console.log(elements);
+
+
+
+
+
+
+
+
+// galleryMarkup.append(...elements);
