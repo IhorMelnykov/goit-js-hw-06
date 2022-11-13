36665385e5ec28c4f08e27b4726befd942ea9606
Ihor Melnykov <imelya17@gmail.com>
2022-11-13 21:23:03 +0200

    complete

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+// const colorContainer = document.querySelector(".widget");
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", onButtonChangeColor);
+
+function onButtonChangeColor(event) {
+	let color = getRandomHexColor();
+	body.style.backgroundColor = color;
+	colorSpan.textContent = color;
+}
+
