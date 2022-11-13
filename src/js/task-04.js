13c235c632aa68b4f065a6c58f044e3d6075bad9
Ihor Melnykov <imelya17@gmail.com>
2022-11-13 17:13:37 +0200

    task 4 fixed

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -9,7 +9,34 @@
 //     counterValue
 // }
 
-const counter {
-    value: 0;
-    
-}
+const counterValue = {
+	value: 0,
+	increment() {
+		console.log("increment -> this", this);
+		this.value += 1;
+	},
+	decrement() {
+		console.log("decrement -> this", this);
+		this.value -= 1;
+	},
+
+};
+
+const decrementBtn = document.querySelector("[data-action=\"decrement\"]");
+const incrementBtn = document.querySelector("[data-action=\"increment\"]");
+const valueResult = document.querySelector("#value");
+
+decrementBtn.addEventListener("click", function () {
+	counterValue.decrement();
+	console.log(counterValue);
+
+	valueResult.textContent = counterValue.value;
+});
+
+incrementBtn.addEventListener("click", function () {
+	counterValue.increment();
+	console.log(counterValue);
+
+	valueResult.textContent = counterValue.value;
+});
+
