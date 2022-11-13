5b9876dcd4fe8006a016342caa264b55c235a77a
Ihor Melnykov <imelya17@gmail.com>
2022-11-13 21:01:22 +0200

    task 8 done

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,35 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+const form = document.querySelector(".login-form");
+const inputEmail = document.querySelector("input[name='email']");
+const inputPassword = document.querySelector("input[name='password']");
+const submitBtn = document.querySelector("button");
 
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+
+	// console.dir(formElements);
+
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+    
+	if (email.length === 0 || password.length === 0) {
+		alert("все поля должны быть заполнены");
+	}
+
+    
+	const formData = {
+		email,
+		password,
+	};
+    
+	form.reset();
+
+    
+	console.log(formData);
+}
